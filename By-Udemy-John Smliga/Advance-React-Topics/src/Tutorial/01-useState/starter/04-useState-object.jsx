34aabea8ebe159ae0470d7c4f401a361686c946c
Jayesh Kumar Verma <111409049+jayeshverma001@users.noreply.github.com>
2023-09-23 23:15:29 +0530

    Add files via upload

---
+++ By-Udemy-John Smliga/Advance-React-Topics/src/Tutorial/01-useState/starter/04-useState-object.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const UseStateObject = () => {
+  const [person,setPerson] = useState({
+    name:'peter',
+    age:24,
+    hobby:'read books',
+  });
+
+  const displayPerson = () => {
+    // setPerson({name:'john',age:28,hobby:'scream at the computer'});
+    // setPerson('shakeAndBake');
+    // setPerson({name: 'susan'});
+    setPerson({...person,name:'susan'});
+  };
+  return(
+   <>
+  <h3>{person.name}</h3>
+  <h3>{person.age}</h3>
+  <h3>Enjoys : {person.hobby}</h3>
+  <button className='btn' onClick={displayPerson}>
+    show john
+  </button>
+  </>
+  );
+};
+
+export default UseStateObject;
